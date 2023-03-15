7d51b548481de2ee6989bf271a19125b1487ff91
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-03-15 16:04:28 +0100

    ADD backend middleware to connect front and back

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+};
 
 /* ROUTE IMPORT */
 const products = require('./routes/product');
 
 const app = express();
 
+/* MIDDLEWARE */
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 /* API */
 app.get('/', (req, res) => {
-    res.status(200).send("Hello! This is the API!");
+    res.status(200).send("Le site web pour les abonnements des tuyaux informatiques!");
 });
 
 app.use('/products', products);
--- frontend/src/components/indexpage/IndexPage.js
+++ frontend/src/components/indexpage/IndexPage.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Axios from 'axios';
+import {AXIOS_CONFIG, INDEX_URL} from '../utils/Constants';
 //import './FrontPage.css';
 
 function IndexPage(){
+    const [title, setTitle] = useState("");
+
+    const getIndexHello = async () => {
+        Axios.get(INDEX_URL)
+        .then((response) => {
+            setTitle(response.data)
+        });
+    };
+
+    useEffect(() => {
+        getIndexHello();
+    }, []);
+    
     return(
         <div className="App front-page">
-            <h1 className="title front-page">Welcome to Informateur</h1>
+            <h1 className="title front-page">Bienvenue chez Informateur</h1>
             <div className="card">
                 <div className="card-body">
                     <p className="card-body">
-                        Le site web pour les abonnements des tuyaux informatiques!
+                        {title}
                     </p>
                 </div>
             </div>
