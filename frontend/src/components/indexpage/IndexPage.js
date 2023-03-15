25ac1aba5947afa69eeb9d1abf7b3b05c89fa197
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-03-15 13:49:32 +0100

    ADD index page

---
+++ frontend/src/components/indexpage/IndexPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import './FrontPage.css';
+
+function IndexPage(){
+    return(
+        <div className="App front-page">
+            <h1 className="title front-page">Welcome to Informateur</h1>
+            <div className="card bg-dark">
+                <div className="card-body">
+                    <p className="card-body">
+                        Le site web pour les abonnements des tuyaux informatiques!
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default IndexPage;
