164ab6cd3cdc1648db580c6b7c4ac6a0f85edc39
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-05-04 00:25:34 +0200

    ADD remove item button on basket list

--- frontend/src/components/BasketList/BasketList.js
+++ frontend/src/components/BasketList/BasketList.js
@@ -1,17 +1,27 @@
 import './BasketList.css';
 import React from 'react';
-import { Container, Card, ListGroup, Col } from 'react-bootstrap';
+import { Button, Container, Card, ListGroup, Col, Row } from 'react-bootstrap';
 
 function BasketList(props) {
     const items = props.basket.map((item) => 
         <ListGroup.Item key={item.ItemIndex}>
-            <Col>{item.ProductName} * {item.ItemQuantity}</Col>
-            <Col>{item.ProductPrice * item.ItemQuantity}</Col>
+            <Row>
+                <Col>{item.ProductName}</Col>
+                <Col>QTY: {item.ItemQuantity}</Col>
+                <Col>{item.ProductPrice * item.ItemQuantity}€</Col>
+                <Col>
+                    <Button 
+                        onClick={() => props.removeItem(item.ItemIndex)}
+                    >
+                        Delete Item
+                    </Button>
+                </Col>
+            </Row>
         </ListGroup.Item>
     );
 
     const subtotal = props.basket.reduce((acc, obj) => acc + (obj.ProductPrice * obj.ItemQuantity), 0);
-
+    
     return(
         <div className='basketList'>
             <Container className='basketList-list'>
@@ -28,7 +38,7 @@ function BasketList(props) {
                                     {items}
                                 </ListGroup>
                             </Card.Body>
-                            <p className="text-right">Subtotal: {subtotal}</p>  
+                            <p className="text-right">Subtotal: {subtotal}€</p>  
                         </Card>
                     </>
                 }
