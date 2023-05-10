d461669cfc9afc6b5361a425408528b9e575b9ce
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-05-10 17:24:29 +0200

    MODIFY Unit Test

--- frontend/src/components/BasketList/BasketList.test.js
+++ frontend/src/components/BasketList/BasketList.test.js
@@ -3,10 +3,12 @@ import BasketList from './BasketList';
 
 describe('Component rendering test <BasketList />', () => {
     it('Should render without crash', async () => {
-      const FakeBasket = {ItemIndex: 1, ProductName: 'Test', ProductPrice: '1', ItemQuantity: '1'}
+      const FakeBasket = [{ItemIndex: 1, ProductName: 'Test', ProductPrice: '1', ItemQuantity: '1'}]
 
-      render(
-            <BasketList products={FakeBasket}/>
+      await render(
+        <>
+          <BasketList basket={FakeBasket}/>
+        </>
         )
     });
 
