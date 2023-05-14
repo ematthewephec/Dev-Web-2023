94efb0104656a7405edd733b5fa3176cad3007b8
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-05-15 01:25:00 +0200

    MODIFY API calls with try catch

--- backend/routes/basket.js
+++ backend/routes/basket.js
@@ -7,28 +7,48 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:userId', async(req, res) => {
-    const userId = req.params.userId;
-    const basketQuery = 'SELECT ItemIndex, Products.ProductName, \
-    Products.ProductPrice, ItemQuantity \
-    FROM Baskets INNER JOIN Users ON Baskets.UserID=Users.UserID INNER JOIN \
-    Products ON Baskets.ProductID=Products.ProductID WHERE Baskets.UserID=?';
-    const result = await pool.query(basketQuery, userId);
-    res.status(200).json(result);
+    try {
+        const userId = req.params.userId;
+        const basketQuery = 'SELECT ItemIndex, Products.ProductName, \
+        Products.ProductPrice, ItemQuantity \
+        FROM Baskets INNER JOIN Users ON Baskets.UserID=Users.UserID INNER JOIN \
+        Products ON Baskets.ProductID=Products.ProductID WHERE Baskets.UserID=?';
+        const result = await pool.query(basketQuery, userId);
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(404).send('User not found!');
+    }
 });
 
 router.delete('/:userId', async(req, res) => {
-    const userId = req.params.userId;
-    const itemIndex = req.body.itemIndex;
-    const basketQuery = 'DELETE FROM Baskets WHERE UserID=? AND ItemIndex=?';
-    const result = await pool.query(basketQuery, [userId, itemIndex]);
-    res.status(200).json(result);
+    try{
+        const userId = req.params.userId;
+        const itemIndex = req.body.itemIndex;
+        const basketQuery = 'DELETE FROM Baskets WHERE UserID=? AND ItemIndex=?';
+        const result = await pool.query(basketQuery, [userId, itemIndex]);
+        result.affectedRows === 0 ?
+            (await pool.query('SELECT * FROM Users WHERE UserID=?', userId)).length === 0 ?
+            res.status(404).send('User not found!') :
+            res.status(404).send('Item not found!') :
+            res.status(200).json(result);
+    } catch (err) {
+        res.status(404).send('Not found!');
+    }
 })
 
 router.delete('/:userId/clear', async(req, res) => {
-    const userId = req.params.userId;
-    const basketQuery = 'DELETE FROM Baskets WHERE UserID=?';
-    const result = await pool.query(basketQuery, userId);
-    res.status(200).json(result);
+    try {
+        const userId = req.params.userId;
+        const basketQuery = 'DELETE FROM Baskets WHERE UserID=?';
+        const result = await pool.query(basketQuery, userId);
+        result.affectedRows === 0 ?
+            (await pool.query('SELECT * FROM Users WHERE UserID=?', userId)).length === 0 ?
+            res.status(404).send('User not found!') :
+            res.status(404).send('No items found in basket!') :
+            res.status(200).json(result);
+    } catch (err) {
+        res.status(404).send('Not found!');
+    }
 })
 
 module.exports = router;
