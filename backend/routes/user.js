385c7a7132306179b46ffa7b5d60ddba5e244b60
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-05-22 01:50:27 +0200

    ADD user deletion method with patch

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,8 @@ const router = express.Router();
 //const config = require('../config/auth.conf')
 const pool = require('../helpers/database');
 const bcrypt = require('bcrypt');
-//const bodyParser = require('body-parser');
 const saltRounds = 10;
+const getCurrentDate = require('../helpers/utils');
 //const jwt = require('jsonwebtoken');
 
 router.get('/', async (req, res) => {
@@ -30,7 +30,7 @@ router.post('/', async (req, res) => {
       if (emailRows.length > 0) {
         res.status(409).json({message: 'Email already registered'});
       } else {
-        const creationDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        const creationDate = getCurrentDate();
         const encryptedPass = await bcrypt.hash(password, saltRounds);
         const registerQuery = 'INSERT INTO Users(UserName, UserEmail, UserPassword, CreationDate) VALUES (?,?,?,?)';
         const result = await pool.query(registerQuery, [username, email, encryptedPass, creationDate]);
@@ -39,6 +39,25 @@ router.post('/', async (req, res) => {
     } catch (error) {
       res.status(400).send(error.message);
     }
-  });
+});
+
+router.patch('/:id/delete', async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const checkUserQuery = 'SELECT * FROM Users WHERE DeletionDate IS NOT NULL AND UserID=?';
+        const userRows = await pool.query(checkUserQuery, userId);
+
+        if (userRows.length > 0) {
+            res.status(404).json({message: 'User not found.'});
+        } else {
+            const deletionDate = getCurrentDate();
+            const patchUserQuery = 'UPDATE Users SET DeletionDate=? WHERE UserID=?';
+            const result = await pool.query(patchUserQuery, [deletionDate, userId]);
+            res.status(200).json({message: 'User successfully deleted.'});
+        }
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+});
 
 module.exports = router;
