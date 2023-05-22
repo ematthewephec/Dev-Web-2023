0d98986a568d973a60b7924e3c41c9e9a9061590
Gabrielle Cruz <gcruz99.official@gmail.com>
2023-05-22 12:20:19 +0200

    Squashed commit of the following:
    commit 385c7a7132306179b46ffa7b5d60ddba5e244b60
    Author: Gabrielle Cruz <gcruz99.official@gmail.com>
    Date:   Mon May 22 01:50:27 2023 +0200
    
        ADD user deletion method with patch
    
    commit 88e92e748687d7967e1679b4f0b0972352d207f1
    Author: Gabrielle Cruz <gcruz99.official@gmail.com>
    Date:   Mon May 22 01:38:35 2023 +0200
    
        MODIFY user registration creation date
    
    commit 87023461f33bd78e1b6ec64c7ed3272e2ce338db
    Author: Gabrielle Cruz <gcruz99.official@gmail.com>
    Date:   Mon May 22 01:29:30 2023 +0200
    
        ADD user registration and bcrypt
    
    commit d908d4fd6185c42d13a2d9dbdbda50e81859d8fe
    Author: Gabrielle Cruz <gcruz99.official@gmail.com>
    Date:   Sun May 21 13:30:21 2023 +0200
    
        ADD users route to backend index

---
+++ backend/routes/user.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+//const config = require('../config/auth.conf')
+const pool = require('../helpers/database');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const getCurrentDate = require('../helpers/utils');
+//const jwt = require('jsonwebtoken');
+
+router.get('/', async (req, res) => {
+    res.status(200).send("This is the user route of the API!");
+});
+
+router.get('/:id', async(req, res) => {
+    try {
+        const userQuery = 'SELECT * FROM Users WHERE DeletionDate IS NULL AND UserID=?';
+        const rows = await pool.query(userQuery, req.params.id);
+        res.status(200).json(rows);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+      const {username, email, password} = req.body;
+      const checkEmailQuery = 'SELECT UserEmail FROM Users WHERE DeletionDate IS NULL AND UserEmail=?';
+      const emailRows = await pool.query(checkEmailQuery, email);
+  
+      if (emailRows.length > 0) {
+        res.status(409).json({message: 'Email already registered'});
+      } else {
+        const creationDate = getCurrentDate();
+        const encryptedPass = await bcrypt.hash(password, saltRounds);
+        const registerQuery = 'INSERT INTO Users(UserName, UserEmail, UserPassword, CreationDate) VALUES (?,?,?,?)';
+        const result = await pool.query(registerQuery, [username, email, encryptedPass, creationDate]);
+        res.status(200).json({message: 'User registered!'});
+      }
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+});
+
+router.patch('/:id/delete', async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const checkUserQuery = 'SELECT * FROM Users WHERE DeletionDate IS NOT NULL AND UserID=?';
+        const userRows = await pool.query(checkUserQuery, userId);
+
+        if (userRows.length > 0) {
+            res.status(404).json({message: 'User not found.'});
+        } else {
+            const deletionDate = getCurrentDate();
+            const patchUserQuery = 'UPDATE Users SET DeletionDate=? WHERE UserID=?';
+            const result = await pool.query(patchUserQuery, [deletionDate, userId]);
+            res.status(200).json({message: 'User successfully deleted.'});
+        }
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+});
+
+module.exports = router;
