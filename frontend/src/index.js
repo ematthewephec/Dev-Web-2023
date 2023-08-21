0d61f6a588f1c221a0bb7a60ab4ce583a6bef75b
Francois-Wecodx <56149458+FrancoisT97@users.noreply.github.com>
2023-08-21 18:45:37 +0200

    import redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux'; // Importez Provider
+import { createStore } from 'redux'; // Importez createStore
+import rootReducer from './reducer/AuthReducer'; // Importez votre rootReducer
 import App from './pages/app/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Provider>
+);
+
 reportWebVitals();
